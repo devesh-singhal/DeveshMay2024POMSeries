deeb523d4842ab7819d2aaebb846f839006a0671
devesh-singhal <163556989+devesh-singhal@users.noreply.github.com>
2024-10-04 14:05:52 +0530

    Create TestPage.java

---
+++ src/main/java/com/qa/opencart/pages/TestPage.java
@@ -0,0 +1,17 @@
+package com.qa.opencart.tests;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+
+ By loc = By.cssSelector("test") ;
+ 
+	
+	public int getPage() {
+		
+		
+		
+		System.out.println("click on Test");
+		return 0;
+	}
+}
