f983e307f4c099d2650c8cf9a6204a3db3c58645
dsfalcon <devesh@falconfs.com>
2024-10-04 13:34:22 +0530

    added demoPage

---
+++ src/test/java/com/qa/opencart/tests/DemoPage.java
@@ -0,0 +1,16 @@
+package com.qa.opencart.tests;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+
+ By loc = By.id("demo") ;
+ 
+	
+	public void getPage() {
+		
+		
+		
+		System.out.println("click on Demo");
+	}
+}
