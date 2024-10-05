1828166e317716aaf1bdda9bd1a5f9034338256f
dsfalcon <devesh@falconfs.com>
2024-10-05 17:54:19 +0530

    f3 feature

--- src/main/java/feature/FeaturePage.java
+++ src/main/java/feature/FeaturePage.java
@@ -9,9 +9,14 @@ public class FeaturePage {
 	
 	
 	
-	public void f2 () {
+	public void f2() {
 		System.out.println("f2 feature");
 
 	}
 	
+	
+	public void f3() {
+		System.out.println("f3 feature");
+
+	}
 }
