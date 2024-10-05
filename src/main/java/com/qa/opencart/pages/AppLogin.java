cca2474bc2b10ef54a845da682d9f97636f3d0ff
dsfalcon <devesh@falconfs.com>
2024-10-05 18:18:28 +0530

    added devesh's code

--- src/main/java/com/qa/opencart/pages/AppLogin.java
+++ src/main/java/com/qa/opencart/pages/AppLogin.java
@@ -5,6 +5,10 @@ public class AppLogin {
 	String name = "Veena";
 	public void login() {
 		System.out.println("Veena----login");
+		System.out.println("DEVESH----login");
+		
+		String un = "deveshautomation" ;
+		
 		
 	}
 
